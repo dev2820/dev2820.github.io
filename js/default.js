de1da4007f2737ff48fd77bc08c1ac6219580364
dev2820 <second28200@naver.com>
2021-01-18 17:17:35 +0900

    build

--- js/default.js
+++ js/default.js
@@ -21,7 +21,6 @@ function scrollToTop() {
     let position = document.body.scrollTop || document.documentElement.scrollTop;
     let scrollAnimation = null;
 
-   
     if (position) {
         window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
         scrollAnimation = setTimeout("scrollToTop()", 10);
