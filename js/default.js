6f6f87a9fbf904a372a36555652a7f97d5dbd65b
dev2820 <second28200@gmail.com>
2021-03-16 21:57:34 +0900

    build

--- js/default.js
+++ js/default.js
@@ -5,18 +5,7 @@ function toggleAside(bool) {
     else
         e.classList.add('show');
 }
-//read json file
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
+
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
@@ -25,21 +14,6 @@ function scrollToBottom() {
 }
 function goToUrl(url) {
     window.location.href = url;
-}
-function makeClockWork() {
-    const deg = 6;
-    const hr = document.querySelector('.clock #hr');
-    const mn = document.querySelector('.clock #mn');
-    const sc = document.querySelector('.clock #sc');
-    setInterval(()=> {
-        let day = new Date();
-        let hh = day.getHours() * 30;
-        let mm = day.getMinutes() * deg;
-        let ss = day.getSeconds() * deg;
-        if(hr) hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-        if(mn) mn.style.transform = `rotateZ(${mm}deg)`;
-        if(sc) sc.style.transform = `rotateZ(${ss}deg)`;
-    },1000)
 }
 window.onload = function () {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
